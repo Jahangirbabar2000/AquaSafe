73ca1da0516a9f363ce32afa35b32886a65a4fff
Usman Tahir <tis.usman2000@gmail.com>
2023-02-21 12:44:22 +0500

    conflict

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,25 +6,12 @@ import Device from "./Components/devices/devices.js"
 import AddSensor from "./Components/Sensor/addSensor";
 import NewProject from "./Components/NewProjectForm/NewProject.js"
 import ProjectMenu from "./Components/projects-menu/projects.js";
-<<<<<<< HEAD
-
-=======
->>>>>>> project-menu
 
 function App() {
   return (
     <div className="main">
       {/* <AddSensor /> */}
-<<<<<<< HEAD
       <Routes />
-=======
-
-      <div className="container1">
-      <div>
-      <Routes />
-      </div>
-      </div >
->>>>>>> project-menu
       {/* <Footer /> */}
     </div>
   );
