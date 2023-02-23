01592b7b527c96b6ee1dcdcbb2d795774b7c4a54
Jahangirbabar2000 <jahangirbabar2000@gmail.com>
2023-02-23 19:26:55 +0500

    ahh

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,13 @@
 import * as React from 'react';
-import Footer from './Components/footer/Footer'
+
 import "./App.css";
 import Routes from './Components/routes';
-import Device from "./Components/devices/devices.js"
-import AddSensor from "./Components/Sensor/addSensor";
-import NewProject from "./Components/NewProjectForm/NewProject.js"
-import ProjectMenu from "./Components/projects-menu/projects.js";
-<<<<<<< HEAD
 
-=======
->>>>>>> project-menu
 
 function App() {
   return (
     <div className="main">
-      {/* <AddSensor /> */}
-<<<<<<< HEAD
       <Routes />
-=======
-
-      <div className="container1">
-      <div>
-      <Routes />
-      </div>
-      </div >
->>>>>>> project-menu
-      {/* <Footer /> */}
     </div>
   );
 }
--- my-app/src/Components/sites/sites.js
+++ my-app/src/Components/sites/sites.js
@@ -13,193 +13,115 @@ import Sidebar from '../sidebar/side-bar';
 import "./sites.css";
 import Navbar from "../navbar/navbar.js"
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
 import Button from '@mui/material/Button';
-=======
-import ProjectMenu from '../projects-menu/projects.js';
->>>>>>> 8e95985a1d20e42465a8033c39df454ed3a39802
 
 function App(props) {
 
-    React.useEffect(() => {     // CODE FOR FIXING MARKER PROBLEM ON MAP
-        const L = require("leaflet");
+  React.useEffect(() => {     // CODE FOR FIXING MARKER PROBLEM ON MAP
+    const L = require("leaflet");
 
-        delete L.Icon.Default.prototype._getIconUrl;
+    delete L.Icon.Default.prototype._getIconUrl;
 
-        L.Icon.Default.mergeOptions({
-            iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-            iconUrl: require("leaflet/dist/images/marker-icon.png"),
-            shadowUrl: require("leaflet/dist/images/marker-shadow.png")
-        });
-    }, []);
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+    });
+  }, []);
 
-    const [inputText, setInputText] = React.useState("");
-    let inputHandler = (e) => { // CODE FOR IMPLEMENTATOIN OF SEARCH BAR
-        //convert input text to lower case
-        var lowerCase = e.target.value.toLowerCase();
-        setInputText(lowerCase);
-    };
+  const [inputText, setInputText] = React.useState("");
+  let inputHandler = (e) => { // CODE FOR IMPLEMENTATOIN OF SEARCH BAR
+    //convert input text to lower case
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
 
-    return (
-<<<<<<< HEAD
-        <div>
-            <Navbar />
-            <Sidebar name="sites" />
-            <Grid container pl={40} pt={6} pb={10} >
-                <Grid item xs={3} > {/* Left small section*/}
-                    <h2 className="projectName">Sites / Projects </h2>
-                    <div className="search" >
-                        <TextField
-                            fullWidth
-                            className="inputRounded"
-                            onChange={inputHandler}
-                            id="input-with-icon-textfield"
-                            label="Search Project"
-                            size='small'
-                            InputProps={{
-                                endAdornment: (
-                                    <InputAdornment position="end">
-                                        <SearchIcon />
-                                    </InputAdornment>
-                                ),
-                            }}
-                            variant="outlined"
-                        />
-                    </div>
+  return (
+    <div>
+      <Navbar />
+      <Sidebar name="sites" />
+      <Grid container pl={40} pt={6} pb={10} >
+        <Grid item xs={3} > {/* Left small section*/}
+          <h2 className="projectName">Sites / Projects </h2>
+          <div className="search" >
+            <TextField
+              fullWidth
+              className="inputRounded"
+              onChange={inputHandler}
+              id="input-with-icon-textfield"
+              label="Search Project"
+              size='small'
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+              variant="outlined"
+            />
+          </div>
 
-                    {/************ Countries/Pakistan ************/}
+          {/************ Countries/Pakistan ************/}
 
-                    <Box sx={{ paddingTop: 5, maxWidth: 220, }}>
-                        <Card variant="outlined" sx={{
-                            maxWidth: 200,
-                            border: '3px solid #30b1d9',
-                            borderRadius: '8px',
-                            ':hover': {
-                                boxShadow: 20, // theme.shadows[20]
-                            },
-                        }}>
-                            <React.Fragment>
-                                <CardContent>
-                                    <h3 class="ul">Pakistan</h3>
+          <Box sx={{ paddingTop: 5, maxWidth: 220, }}>
+            <Card variant="outlined" sx={{
+              maxWidth: 200,
+              border: '3px solid #30b1d9',
+              borderRadius: '8px',
+              ':hover': {
+                boxShadow: 20, // theme.shadows[20]
+              },
+            }}>
+              <React.Fragment>
+                <CardContent>
+                  <h3 class="ul">Pakistan</h3>
 
-                                    <List sx={{
-                                        width: '100%', maxWidth: 360,
-                                        bgcolor: 'background.paper'
-                                    }} input={inputText} />
-                                </CardContent>
-                            </React.Fragment>
-                        </Card>
-                    </Box>
-                </Grid>
-                <Grid item sm={9} alignItems="flex=end"
-                    justifyContent="flex=end">
-                    <MapContainer className='siteMap' center={[33.702299, 73.130]} zoom={11} scrollWheelZoom={true}>
-                        <TileLayer
-                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        <Marker position={[33.702299, 73.130]}>
-                            <Popup>
-                                Location of this Project: <br /> Rawal Lake, Islamabad
-                            </Popup>
-                        </Marker>
-                    </MapContainer>
-
-                </Grid>
-            </Grid>
-            <Grid container pl={150}>
-                <Link to="/dashboard">
-                    <Button variant="contained">
-                        View Devices
-                    </Button>
-                </Link>
-            </Grid>
-        </div>
-=======
-      <div>
-        <Navbar />
-        <Sidebar name="sites" />
-            <div className="menuContainer"><ProjectMenu /> </div>
-        <Grid container pl={40} pt={6} pb={10}>
-          <Grid item xs={3}>
-            {" "}
-            {/* Left small section*/}
-            <h2 className="projectName">Sites / Projects </h2>
-            <div className="search">
-              <TextField
-                fullWidth
-                className="inputRounded"
-                onChange={inputHandler}
-                id="input-with-icon-textfield"
-                label="Search Project"
-                size="small"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <SearchIcon />
-                    </InputAdornment>
-                  )
-                }}
-                variant="outlined"
-              />
-            </div>
-            {/************ Countries/Pakistan ************/}
-            <Box sx={{ paddingTop: 5, maxWidth: 220 }}>
-              <Card
-                variant="outlined"
-                sx={{
-                  maxWidth: 200,
-                  border: "3px solid #30b1d9",
-                  borderRadius: "8px",
-                  ":hover": {
-                    boxShadow: 20 // theme.shadows[20]
-                  }
-                }}
-              >
-                <React.Fragment>
-                  <CardContent>
-                    <h3 class="ul">Pakistan</h3>
->>>>>>> 8e95985a1d20e42465a8033c39df454ed3a39802
-
-                    <List
-                      sx={{
-                        width: "100%",
-                        maxWidth: 360,
-                        bgcolor: "background.paper"
-                      }}
-                      input={inputText}
-                    />
-                  </CardContent>
-                </React.Fragment>
-              </Card>
-            </Box>
-          </Grid>
-          <Grid item sm={9} alignItems="flex=end" justifyContent="flex=end">
-            <MapContainer
-              className="siteMap"
-              center={[33.702299, 73.13]}
-              zoom={11}
-              scrollWheelZoom={true}
-            >
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Marker position={[33.702299, 73.13]}>
-                <Popup>
-                  Location of this Project: <br /> Rawal Lake, Islamabad
-                </Popup>
-              </Marker>
-            </MapContainer>
-          </Grid>
+                  <List sx={{
+                    width: '100%', maxWidth: 360,
+                    bgcolor: 'background.paper'
+                  }} input={inputText} />
+                </CardContent>
+              </React.Fragment>
+            </Card>
+          </Box>
         </Grid>
-        <Grid container pl={150}>
-          <Link to="/dashboard">
-            <NewButton text={"View Devices"}></NewButton>
-          </Link>
+        <Grid item sm={9} alignItems="flex=end"
+          justifyContent="flex=end">
+          <MapContainer className='siteMap' center={[33.702299, 73.130]} zoom={11} scrollWheelZoom={true}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[33.702299, 73.130]}>
+              <Popup>
+                Location of this Project: <br /> Rawal Lake, Islamabad
+              </Popup>
+            </Marker>
+          </MapContainer>
+
         </Grid>
-      </div>
-    );
+      </Grid>
+      <Grid container pl={150}>
+        <Link to="/dashboard">
+          <Button variant="contained">
+            View Devices
+          </Button>
+        </Link>
+      </Grid>
+
+
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper"
+        }}
+        input={inputText}
+      />
+
+    </div >
+  );
 }
 
 export default App;
