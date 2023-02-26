685126e3e22db58635a6553f3910a72553faf678
Jahangirbabar2000 <jahangirbabar2000@gmail.com>
2023-02-26 11:59:56 +0500

    backup

--- my-app/src/Components/sites/projects-menu/projects.js
+++ my-app/src/Components/sites/projects-menu/projects.js
@@ -1,9 +1,9 @@
-import "./projects.css";
-import * as React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import IconButton from "@material-ui/core/IconButton";
 import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
 import Typography from "@material-ui/core/Typography";
+import "./projects.css";
 
 const data = {
   countries: [
@@ -12,130 +12,151 @@ const data = {
       states: [
         {
           name: "Islamabad",
-          cities: ["Rawal Lake", "Nust Lake"]
+          lakes: [
+            { name: "Rawal Lake", coordinates: '33.704196, 73.127151' },
+            { name: "Nust Lake", coordinates: '33.639924, 72.991497' }
+          ]
         },
         {
           name: "Lahore",
-          cities: ["Lahore Canal"]
+          lakes: [
+            { name: "Lahore Canal", coordinates: '31.456612, 74.225711' }
+          ]
         }
       ]
     },
     {
       name: "India",
       states: [
-        { name: "Delhi", cities: ["Yamuna River", "Sanjay Lake"] },
-        { name: "Mumbai", cities: ["Tulsi Lake", "Pawai Lake"] }
+        {
+          name: "Delhi",
+          lakes: [
+            { name: "Yamuna River", coordinates: '26.919072, 78.603781' },
+            { name: "Sanjay Lake", coordinates: '28.614334, 77.299822' }
+          ]
+        },
+        {
+          name: "Mumbai",
+          lakes: [
+            { name: "Tulsi Lake", coordinates: '19.191272, 72.917397' },
+            { name: "Pawai Lake", coordinates: '19.126180, 72.902860' }
+          ]
+        }
       ]
     },
     {
       name: "Bangladesh",
       states: [
-        { name: "Dhaka", cities: ["Turag River", "Gulshan Lake"] },
-        { name: "Chattogram", cities: ["Sangu River", "Kaptai Lake"] }
+        {
+          name: "Dhaka",
+          lakes: [
+            { name: "Turag River", coordinates: '23.908079, 90.333078' },
+            { name: "Hatir Jheel", coordinates: '23.768993, 90.416165' }
+          ]
+        },
+        {
+          name: "Chattogram",
+          lakes: [
+            { name: "Sangu River", coordinates: '21.836432, 92.417387' },
+            { name: "Kaptai Lake", coordinates: '22.673598, 92.161764' }
+          ]
+        }
       ]
-    }
-
+    },
   ]
 };
-export default function ProjectMenu() {
-  const [selectedCountry, setSelectedCountry] = React.useState();
-  const [selectedCity, setSelectedCity] = React.useState();
-  const [selectedProject, setSelectedProject] = React.useState();
+
+function ProjectMenu(props) {
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [selectedProject, setSelectedProject] = useState("");
 
   const availableState = data.countries.find((c) => c.name === selectedCountry);
   const availableProjects = availableState?.states?.find(
     (s) => s.name === selectedCity
   );
 
-  function handleClick() {
-    console.log("Country: ", selectedCountry);
-    console.log("City: ", selectedCity);
-    console.log("Project: ", selectedProject);
-  }
 
-  function handleClick1() {
+
+  const handleClick = () => {
+    console.log("HIIIIIII");
+  };
+
+  const handleClick1 = () => {
     setSelectedCity("");
     setSelectedCountry("");
     setSelectedProject("");
-  }
+  };
 
   return (
     <div id="container-projects">
-      {/* <p style={{ color: "white", fontSize: "2em", paddingRight: 75 }}>
-        Search Project
-      </p> */}
       <div>
         <select
           className="select-projects"
           placeholder="Country"
           value={selectedCountry}
-          onChange={e => setSelectedCountry(e.target.value)}
+          onChange={(e) => setSelectedCountry(e.target.value)}
           style={{ fontSize: 12 }}
         >
           <option className="option-projects" value="" disabled selected>
             Select Country
           </option>
-          {data.countries.map((value, key) => {
-            return (
-              <option className="option-projects" value={value.name} key={key}>
-                {value.name}
-              </option>
-            );
-          })}
+          {data.countries.map((value, key) => (
+            <option className="option-projects" value={value.name} key={key}>
+              {value.name}
+            </option>
+          ))}
         </select>
       </div>
 
       <div>
-        {/* <label>City</label> */}
         <select
           className="select-projects"
           style={{ fontSize: 12 }}
           placeholder="City"
           value={selectedCity}
-          onChange={e => setSelectedCity(e.target.value)}
+          onChange={(e) => setSelectedCity(e.target.value)}
+          disabled={!selectedCountry}
         >
           <option className="option-projects" value="" disabled selected>
             Select City
           </option>
-          {availableState?.states.map((e, key) => {
-            return (
-              <option className="option-projects" value={e.name} key={key}>
-                {e.name}
-              </option>
-            );
-          })}
+          {availableState?.states.map((value, key) => (
+            <option className="option-projects" value={value.name} key={key}>
+              {value.name}
+            </option>
+          ))}
         </select>
       </div>
-
       <div>
-        {/* <label>Project</label> */}
-
         <select
           className="select-projects"
           style={{ fontSize: 12 }}
           placeholder="Project"
           value={selectedProject}
-          onChange={e => setSelectedProject(e.target.value)}
+          onChange={(e) => {
+            setSelectedProject(e.target.value);
+            const selectedLake = availableProjects?.lakes.find(lake => lake.name === e.target.value);
+            if (selectedLake) {
+              props.setSelectedCoordinates(selectedLake.coordinates.split(',').map(coord => Number(coord)));
+            } else {
+              props.setSelectedCoordinates([]);
+            }
+          }}
+          disabled={!selectedCity}
         >
           <option className="option-projects" value="" disabled selected>
             Select Project
           </option>
-          {availableProjects?.cities.map((e, key) => {
-            return (
-              <option className="option-projects" value={e.name} key={key}>
-                {e}
-              </option>
-            );
-          })}
+          {availableProjects?.lakes.map((lake, key) => (
+            <option className="option-projects" value={lake.name} key={key}>
+              {lake.name}
+            </option>
+          ))}
         </select>
       </div>
-      <div
-        id="clearButton"
-        style={{
-          display: "flex",
-          flexDirection: "row"
-        }}
-      >
+
+      <div className="buttons-div">
         <IconButton aria-label="delete" onClick={handleClick1}>
           <CancelRoundedIcon className="icon" size="small" />
           <Typography className="icon" variant="body1">
@@ -143,15 +164,15 @@ export default function ProjectMenu() {
             clear
           </Typography>
         </IconButton>
+        <Button
+          variant="contained"
+          onClick={handleClick}
+        >
+          Show Coordinates
+        </Button>
       </div>
-      <Button
-        variant="contained"
-        size="large"
-        sx={{ marginTop: 0.1, marginLeft: "1%", fontSize: 12 }}
-        onClick={handleClick}
-      >
-        Search Project
-      </Button>
     </div>
   );
 }
+
+export default ProjectMenu;
--- my-app/src/Components/sites/sites.js
+++ my-app/src/Components/sites/sites.js
@@ -1,59 +1,84 @@
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css';
-import Sidebar from '../sidebar/side-bar';
+import React, { useState, useEffect, useRef } from "react";
+import Grid from "@mui/material/Grid";
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet'
+import "leaflet/dist/leaflet.css";
+import Sidebar from "../sidebar/side-bar";
 import "./sites.css";
-import Navbar from "../navbar/navbar.js"
-import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
-import ProjectMenu from '../projects-menu/projects';
-function App(props) {
+import Navbar from "../navbar/navbar.js";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import ProjectMenu from "./projects-menu/projects";
 
-    React.useEffect(() => {     // CODE FOR FIXING MARKER PROBLEM ON MAP
-        const L = require("leaflet");
+function Sites() {
+  const [selectedCoordinates, setSelectedCoordinates] = useState([31.99, 77.13]);
+  const mapRef = useRef();
 
-        delete L.Icon.Default.prototype._getIconUrl;
+  useEffect(() => {
+    // CODE FOR FIXING MARKER PROBLEM ON MAP
+    const L = require("leaflet");
 
-        L.Icon.Default.mergeOptions({
-            iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-            iconUrl: require("leaflet/dist/images/marker-icon.png"),
-            shadowUrl: require("leaflet/dist/images/marker-shadow.png")
-        });
-    }, []);
+    delete L.Icon.Default.prototype._getIconUrl;
 
-    return (        
-      <div>
-        <Navbar />
-        <Sidebar name="sites" />
-            <div className="menuContainer"><ProjectMenu /> </div>
-        <Grid container pl={40} pt={6} pb={10}>          
-          <Grid item sm={9} alignItems="flex=end" justifyContent="flex=end">
-            <MapContainer
-              className="siteMap"
-              center={[33.702299, 73.13]}
-              zoom={11}
-              scrollWheelZoom={true}
-            >
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Marker position={[33.702299, 73.13]}>
-                <Popup>
-                  Location of this Project: <br /> Rawal Lake, Islamabad
-                </Popup>
-              </Marker>
-            </MapContainer>
-          </Grid>
-        </Grid>
-        <Grid container pl={85}>
-          <Link to="/dashboard">
-          <Button sx={{fontSize:16}} fullWidth variant="contained">View Devices</Button>
-          </Link>
-        </Grid>
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+    });
+
+    // update map center when selectedCoordinates changes
+    if (mapRef.current && selectedCoordinates && selectedCoordinates.length === 2) {
+      const [lat, lng] = selectedCoordinates;
+      mapRef.current.setView([lat, lng], 11);
+      mapRef.current.invalidateSize();
+    }
+  }, [selectedCoordinates, mapRef]);
+
+
+
+
+  return (
+    <div>
+      <Navbar />
+      <Sidebar name="sites" />
+      <div className="menuContainer">
+        <ProjectMenu setSelectedCoordinates={setSelectedCoordinates} />
       </div>
-    );
+      <Grid container pl={40} pt={6} pb={10}>
+        <Grid item sm={9} alignItems="flex=end" justifyContent="flex=end">
+          <MapContainer
+            className="siteMap"
+            center={selectedCoordinates}
+            zoom={11}
+            scrollWheelZoom={true}
+            whenCreated={(map) => {
+              // store map object in ref
+              mapRef.current = map;
+            }}
+          >
+            {console.log('MapContainer re-rendered')}
+            {console.log('Center prop:', selectedCoordinates)}
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={selectedCoordinates}>
+              <Popup>
+                Location of this Project: <br /> Rawal Lake, Islamabad
+              </Popup>
+            </Marker>
+
+          </MapContainer>
+        </Grid>
+      </Grid>
+      <Grid container pl={85}>
+        <Link to="/dashboard">
+          <Button sx={{ fontSize: 16 }} fullWidth variant="contained">
+            View Devices
+          </Button>
+        </Link>
+      </Grid>
+    </div>
+  );
 }
 
-export default App;
+export default Sites;
