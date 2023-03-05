c1e6e38f72712438942cb74e4410763cb28af8db
Jahangirbabar2000 <jahangirbabar2000@gmail.com>
2023-03-06 00:30:19 +0500

    d

--- my-app/src/Components/visualization/visualization.js
+++ my-app/src/Components/visualization/visualization.js
@@ -38,7 +38,7 @@ function App(props) {
       iconUrl: require("leaflet/dist/images/marker-icon.png"),
       shadowUrl: require("leaflet/dist/images/marker-shadow.png")
     });
-    axios.get(`/hkdata`)
+    axios.get(`http://localhost:8080/hkdata`)
       .then(response => {
         setData(response.data);
         setLoading(false);
@@ -49,7 +49,7 @@ function App(props) {
   const getData = () => {
     const sd = moment(startDate, 'ddd MMM DD YYYY HH:mm:ss [GMT]ZZ').format('YYYY-MM-DD HH:mm:ss');
     const ed = moment(endDate, 'ddd MMM DD YYYY HH:mm:ss [GMT]ZZ').format('YYYY-MM-DD HH:mm:ss');
-    axios.get(`/hkdata2?start_date=${sd}&end_date=${ed}`)
+    axios.get(`http://localhost:8080/hkdata2?start_date=${sd}&end_date=${ed}`)
       .then(response => {
         setData(response.data);
         setLoading(false);
