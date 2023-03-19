04fbebed9998d526af8ab380295a76fb440f2f9d
root <root@ip-172-31-52-51.ec2.internal>
2023-03-19 11:45:37 +0000

    lol

---
+++ my-app/src/Components/NotificationTable/DeviceNotifications.js
@@ -0,0 +1,121 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import "./NotificationTable.css";
+
+const DeviceNotification = (props) => {
+    const data = props.data;
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const columns = [
+        // { field: "id", headerName: "ID", cellClassName: "name-column--cell" },
+        {
+            field: "project",
+            headerName: "Project",
+            flex: 1,
+            cellClassName: "name-column--cell"
+        },
+        {
+            field: "country",
+            headerName: "Country",
+            cellClassName: "name-column--cell",
+            // type: "number",
+            flex: 1
+            // headerAlign: "left",
+            // align: "left",
+        },
+        {
+            field: "city",
+            headerName: "City",
+            cellClassName: "name-column--cell",
+            // type: "number",
+            flex: 1
+            // headerAlign: "left",
+            // align: "left",
+        },
+        {
+            field: "device",
+            headerName: "Device",
+            cellClassName: "name-column--cell",
+            flex: 1
+        },
+        {
+            field: "status",
+            headerName: "Status",
+            cellClassName: "name-column--cell",
+            // type: "number",
+            // headerAlign: "left",
+            // align: "left"
+            flex: 1
+        },
+        {
+            field: "details",
+            headerName: "Details",
+            // type: "number",
+            // headerAlign: "left",
+            // align: "left"
+            flex: 2,
+            cellClassName: "name-column--cell"
+        },
+        {
+            field: "date",
+            headerName: "Date",
+            cellClassName: "name-column--cell"
+
+            // flex: 1
+        }
+    ];
+
+    return (
+        <div><h1 style={{ marginLeft: "4%", paddingTop: "1%" }}>Device Notifications</h1>
+        <Box m="10px">
+            <Box
+                className="sensor-notfication-box"
+                m="25px 0 0 3%"
+                height="75vh"
+                sx={{
+                    width: "95%",
+                    "& .MuiDataGrid-root": {
+                        // border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        // borderBottom: "none",
+                    },
+                    "& .name-column--cell": {
+                        // color: colors.greenAccent[300],
+                        fontSize: 14
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        //   backgroundColor: colors.blueAccent[700],
+                        backgroundColor: "black",
+                        borderBottom: "none",
+                        fontSize: 15,
+                        color: "white"
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: colors.primary[400]
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        borderTop: "none",
+                        backgroundColor: "black"
+                    },
+                    "& .MuiCheckbox-root": {
+                        color: `${colors.greenAccent[200]} !important`
+                    },
+                    "& .MuiTablePagination-root": {
+                        fontSize: 15,
+                        color: "white"
+                    },
+                    "& .MuiSvgIcon-root": {
+                        color: "white"
+                    }
+                }}
+            >
+                <DataGrid checkboxSelection rows={data} columns={columns} />
+            </Box>
+        </Box>
+        </div>
+    );
+};
+
+export default DeviceNotification;
---
+++ my-app/src/Components/NotificationTable/NotificationTable.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import Sidebar from "../sidebar/side-bar.js";
+import Navbar from "../navbar/navbar.js";
+import Button from "@mui/material/Button";
+import data from "../NotificationBox/notification.json";
+import SensorNotification from "./SensorNotifications";
+import DeviceNotification from "./DeviceNotifications.js"
+import "./NotificationTable.css";
+
+// this function adds details attribute to the json file depending on the error code.
+function AddDetails(data) {
+  const DetailsData = data.map(entry => {
+    if (entry.code === 10) {
+      entry.details = entry.sensor + " is at dangerous level.";
+    } else if (entry.code === 11) {
+      entry.details = entry.sensor + " is close to dangerous level.";
+    } else if (entry.code === 12) {
+      entry.details = entry.sensor + " is at optimal level.";
+    } else if (entry.code === 20) {
+      entry.details = entry.device + " has crashed.";
+    } else if (entry.code === 21) {
+      entry.details = entry.device + " has moved";
+    }
+    return entry;
+  });
+  return DetailsData;
+}
+
+export default function NotificationTable() {
+  const [isDetailData, setDetailData] = useState([]);
+  const [showDevice, setShowDevice] = useState(false);
+
+  React.useEffect(() => {
+    const DetailData = AddDetails(data);
+    setDetailData(DetailData);
+  }, []);
+
+  const SensorNotifications = isDetailData.filter(
+    item => item.type !== "device"
+  );
+  const DeviceNotifications = isDetailData.filter(
+    item => item.type !== "sensor"
+  );
+
+  const handleClick = () => {
+    setShowDevice(true);
+  };
+
+  const handleClick2 = () => {
+    setShowDevice(false);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "32vh auto"
+          // gridGap: "2px"
+        }}
+      >
+        <div>
+          <Sidebar name="notification" />
+        </div>
+        <div>
+          <Button
+            variant="contained"
+            // borderWidth={100}
+            style={{ marginLeft: "4%", padding: "0.5%", marginTop: "10px" }}
+            onClick={handleClick2}
+            color={`${showDevice === false ? "success" : "primary"}`}
+          >
+            Sensor
+          </Button>
+          <Button
+            style={{ marginLeft: "1%", padding: "0.5%", marginTop: "10px" }}
+            variant="contained"
+            // borderWidth={3}
+            onClick={handleClick}
+            color={`${showDevice === true ? "success" : "primary"}`}
+            // sx={{border:"green"}}
+          >
+            Device
+          </Button>
+
+          {showDevice ? (
+            <DeviceNotification data={DeviceNotifications} /> // this will show device notifications
+          ) : (
+            <SensorNotification data={SensorNotifications} /> //this will show sensor notifications
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
