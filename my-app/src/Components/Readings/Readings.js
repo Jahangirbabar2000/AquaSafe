5eb013e4759914bc745c3df37883b9e6b5c57646
Usman Tahir <tis.usman2000@gmail.com>
2023-04-14 11:20:06 +0500

    added upload file and make table of csv

--- my-app/src/Components/Readings/Readings.js
+++ my-app/src/Components/Readings/Readings.js
@@ -3,6 +3,12 @@ import Sidebar2 from "../sidebar/Sidebar2.js";
 import Navbar from "../navbar/navbar.js";
 import Button from "@mui/material/Button";
 import ReadingsTable from "./ReadingsTable.js";
+import CsvTokenizer from "./importCSV.js";
+import Papa from "papaparse";
+
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
 
 import "./Readings.css";
 
@@ -10,17 +16,40 @@ import "./Readings.css";
 // as
 // <ReadingsTable data={JsonObjectName}>
 
-export default function Readings() {
-  const handleClick = () => {
-    console.log("download button");
-  };
 
-  const handleClick2 = () => {
-    console.log("upload button");
+
+
+export default function Readings() {
+  const [jsonData, setJsonData] = useState([]);
+  const [isCSV, setCSV] = useState(false);
+  const [selectedFileName, setSelectedFileName] = useState(null);
+
+  const handleFileChange = e => {
+    const file = e.target.files[0];
+    setSelectedFileName(file.name);
+    Papa.parse(file, {
+      header: true,
+      complete: results => {
+        const rows = results.data;
+        const headers = results.meta.fields;
+        const json = rows.slice(0, -1).map(row =>
+          headers.reduce((acc, header) => {
+            acc[header] = row[header];
+            return acc;
+          }, {})
+        );
+        setJsonData(json);
+        setCSV(true);
+      
+      }
+    });
+    console.log(jsonData);
+      console.log("data hasbeen uploaded");
   };
 
   return (
     <div>
+      {/* <CsvTokenizer /> */}
       <Navbar />
       <div
         style={{
@@ -35,21 +64,175 @@ export default function Readings() {
           <Button
             variant="contained"
             style={{ marginLeft: "1%", padding: "0.5%", marginTop: "10px" }}
-            onClick={handleClick2}
+            // onClick={handleClick2}
           >
             Download Template
           </Button>
-          <Button
-            style={{ marginLeft: "1%", padding: "0.5%", marginTop: "10px" }}
-            variant="contained"
-            onClick={handleClick}
-          >
-            Upload Data
+
+          <Button variant="contained" component="label" style={{ marginLeft: "1%", padding: "0.5%", marginTop: "10px" }}>
+            {selectedFileName ? selectedFileName : "Upload"}
+          <input hidden accept=".csv" type="file" onChange={handleFileChange} />
           </Button>
 
-          <ReadingsTable />
+          {!isCSV &&
+            <ReadingsTable />
+          }
+          {
+            isCSV && <CSVTable data={jsonData}/>
+          }
+         
         </div>
       </div>
     </div>
   );
+}
+
+function CSVTable(props){
+  const data = props.data;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns = [
+    // { field: "id", headerName: "ID", cellClassName: "name-column--cell" },
+    {
+      field: "Location",
+      headerName: "Location",
+      // flex: 2,
+      cellClassName: "name-column--cell"
+    },
+    {
+      field: "Station",
+      headerName: "Station",
+      cellClassName: "name-column--cell",
+      // type: "number",
+      flex: 1,
+      // headerAlign: "left",
+      // align: "left",
+    },
+    {
+      field: "Date",
+      headerName: "Date",
+      cellClassName: "name-column--cell",
+      // type: "number",
+      flex: 1
+      // headerAlign: "left",
+      // align: "left",
+    },
+    {
+      field: "Temperature",
+      headerName: "Temperature (C)",
+      cellClassName: "name-column--cell",
+      flex: 1
+    },
+    {
+      field: "pH",
+      headerName: "pH",
+      cellClassName: "name-column--cell"
+      // flex: 1
+    },
+    {
+      field: "DissolvedOxygen",
+      headerName: "Dissolved Oxygen (mg/L)",
+      flex: 2,
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+      cellClassName: "name-column--cell"
+    },
+    {
+      field: "Conductivity",
+      headerName: "Conductivity (µS/cm)",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+      cellClassName: "name-column--cell",
+      flex: 2
+    },
+    {
+      field: "NitriteNitrogen",
+      headerName: "Nitrite-Nitrogen (mg/L)",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+      cellClassName: "name-column--cell",
+      flex: 2
+    },
+    {
+      field: "BOD5",
+      headerName: "BOD5 (mg/L)",
+      cellClassName: "name-column--cell",
+      // type: "number",
+      // headerAlign: "left",
+      // align: "left"
+      flex: 1
+    },
+    {
+      field: "TotalPhosphorus",
+      headerName: "Total Phosphorus (mg/L)",
+      // type: "number",
+      // headerAlign: "left",
+      // align: "left"
+      flex: 2,
+      cellClassName: "name-column--cell"
+    },
+    {
+      field: "AmmoniaNitrogen",
+      headerName: "Ammonia Nitrogen (mg/L)",
+      cellClassName: "name-column--cell",
+      flex: 2,
+
+      // flex: 1
+    }
+  ];
+
+  return (
+    <div><h1 style={{ marginLeft: "1%", paddingTop: "1%" }}>CSV Readings</h1>
+      <Box m="0x">
+        <Box
+          className="sensor-notfication-box"
+          m="15px 0 0 1%"
+          height="75vh"
+          sx={{
+            width: "95%",
+            "& .MuiDataGrid-root": {
+              // border: "none",
+            },
+            "& .MuiDataGrid-cell": {
+              // borderBottom: "none",
+            },
+            "& .name-column--cell": {
+              // color: colors.greenAccent[300],
+              fontSize: 12
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              //   backgroundColor: colors.blueAccent[700],
+              backgroundColor: "black",
+              borderBottom: "none",
+              fontSize: 15,
+              color: "white"
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: colors.primary[400]
+            },
+            "& .MuiDataGrid-footerContainer": {
+              borderTop: "none",
+              backgroundColor: "black"
+            },
+            "& .MuiCheckbox-root": {
+              color: `${colors.greenAccent[200]} !important`
+            },
+            "& .MuiTablePagination-root": {
+              fontSize: 15,
+              color: "white"
+            },
+            "& .MuiSvgIcon-root": {
+              color: "white"
+            }
+          }}
+        >
+          <DataGrid checkboxSelection rows={data} columns={columns} headerWordWrap={true} headerAlign="center" />
+        </Box>
+      </Box>
+    </div>
+  );
+
 }
